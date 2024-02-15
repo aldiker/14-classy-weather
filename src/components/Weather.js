1a1626204dbea5cee54b3450cedcf341cf5a766c
aldiker <aldiker@ukr.net>
2024-02-15 15:50:00 +0200

    Added Weather and Day components

---
+++ src/components/Weather.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import Day from './Day'
+
+// function getToday(dateStr) {
+//     const today = new Date()
+//     const year = today.getFullYear()
+//     let month = today.getMonth() + 1
+//     month = month < 10 ? '0' + month : month
+//     let day = today.getDate()
+//     day = day < 10 ? '0' + day : day
+
+//     const todayFormated = `${year}-${month}-${day}`
+
+//     return todayFormated === dateStr
+// }
+
+export default class Weather extends React.Component {
+    render() {
+        console.log('rendered')
+        // console.log(this.props)
+        const {
+            temperature_2m_max: max,
+            temperature_2m_min: min,
+            time: date,
+            weathercode: codes,
+        } = this.props.weather
+
+        const today = new Date()
+        const year = today.getFullYear()
+        let month = today.getMonth() + 1
+        month = month < 10 ? '0' + month : month
+        let day = today.getDate()
+        day = day < 10 ? '0' + day : day
+
+        const todayFormated = `${year}-${month}-${day}`
+
+        return (
+            <div>
+                <h2>
+                    Weather - {this.props.location} - {date.at(0)}
+                </h2>
+                <ul className='weather'>
+                    {max.map((el, idx) => (
+                        <Day
+                            max={max[idx]}
+                            min={min[idx]}
+                            date={date[idx]}
+                            code={codes[idx]}
+                            key={idx}
+                            isToday={todayFormated === date[idx]}
+                        />
+                    ))}
+                </ul>
+            </div>
+        )
+    }
+}
